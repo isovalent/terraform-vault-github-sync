87475cc744bb68d0ed50d90aab4738f61ee53fd4
Sekhar Sankaramanchi <sekhar@isovalent.com>
2025-08-13 05:03:57 -0700

    chore: add type of github_svt to output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,7 @@ output "github_svt_path" {
   value = {
     path = "${var.organization}${var.repository != "" ? "/${var.repository}" : ""}${var.environment != "" ? "/${var.environment}" : ""}/${var.github_svt.name}"
     key  = var.github_svt.key
+    type = var.type
 
     created_at = (
       var.type == "variable" ?
